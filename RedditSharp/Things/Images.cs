b93e60021d8d5b7764b86a1995282ee048b59e74
Aeddginvael <aeroewan@gmail.com>
2021-08-08 00:58:46 +0300

    add variants property

--- RedditSharp/Things/Images.cs
+++ RedditSharp/Things/Images.cs
@@ -28,6 +28,9 @@ namespace RedditSharp.Things
         /// </summary>
         [JsonProperty("resolutions")]
         public List<Image> Resolutions { get; private set; }
+        
+        [JsonProperty("variants")]
+        public Variants Variants { get; private set; }
 
         /// <summary>
         /// Post preview id.
